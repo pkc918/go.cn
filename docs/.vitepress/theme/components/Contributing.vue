fbc3a04fc4c7ca0483ea322b96dca50c1e17aa48
pkc918 <xiaopan2000918@gmail.com>
2024-07-06 00:11:00 +0800

    fix: fix style

--- docs/.vitepress/theme/components/Contributing.vue
+++ docs/.vitepress/theme/components/Contributing.vue
@@ -1,20 +1,6 @@
 <script setup lang="ts">
 import { VPHomeSponsors } from 'vitepress/theme'
-
-/* const { data } = useSponsor() */
-const friends = [{
-  avatar: "https://avatars.githubusercontent.com/u/58922004?v=4",
-  githubHome: "https://github.com/pkc918",
-  githubName: "青椒肉丝"
-},{
-  avatar: "https://avatars.githubusercontent.com/u/55641773?v=4",
-  githubHome: "https://github.com/Alfred-Skyblue",
-  githubName: "远方os",
-},{
-  avatar: "https://avatars.githubusercontent.com/u/72547426?v=4",
-  githubHome: "https://github.com/Coder-XiaoZhuang",
-  githubName: "better"
-}]
+import { friends } from "../../../friends";
 </script>
 
 <template>
@@ -26,14 +12,14 @@ const friends = [{
     <p class="w-full flex justify-center items-center text-5 font-600">友友链接！</p>
   </div>
   <ul class="flex flex-wrap w-full sm:w-2/3 md:1/3 mx-auto gap-20">
-    <li class="w-1/8 cursor-pointer" v-for="friend in friends" :key="friend.githubHome" >
+    <li class="cursor-pointer" v-for="{githubHome,avatar,githubName } of friends" :key="githubHome">
       <a
-          :href="friend.githubHome"
+          :href="githubHome"
           target="_blank"
           rel="noreferrer"
       >
-        <img class="rd-full" :src="friend.avatar" alt="">
-        <p class="text-center mt-4 font-500">{{friend.githubName}}</p>
+        <img class="rd-full w-50 h-50" :src="avatar" alt="">
+        <p class="text-center mt-4 font-500">{{ githubName }}</p>
       </a>
     </li>
   </ul>
